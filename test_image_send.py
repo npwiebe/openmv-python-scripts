214fc3d6c2225dd1d159fa14b86c195d9e3e2a2f
npwiebe <npwiebe@gmail.com>
2022-03-13 10:26:55 -0500

    add comments

--- test_image_send.py
+++ test_image_send.py
@@ -17,22 +17,18 @@ sensor.skip_frames(time = 2000)
 
 
 # Always pass UART 3 for the UART number for your OpenMV Cam.
-# The second argument is the UART baud rate. For a more advanced UART control
-# example see the BLE-Shield driver.
 uart = UART(3,9600)
 uart.init(9600, bits=8, parity=None, stop=1, timeout=1000, read_buf_len=64)
 clock = time.clock()
 img = sensor.snapshot().compress(quality=40)
-print(img.size())
 
-imgString = str(binascii.hexlify(img))
-print(imgString)
 
+print(img.size()) #prints image size
+print(str(binascii.hexlify(img))) #print image hex data to terminal
 
 
 uart.write(str(img.size()))
 time.sleep(0.1)
 uart.write(img)
-#uart.write(struct.pack(">l",150))
-#uart.write(byte_array)
+
 #print(clock.fps())
